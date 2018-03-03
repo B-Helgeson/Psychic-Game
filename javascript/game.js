a850569e0c65dfb239ca49c45c9428ae49dae8ea
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-03-02 21:59:54 -0500

    perfected JavaScript

--- javascript/game.js
+++ javascript/game.js
@@ -1,38 +1,102 @@
+window.onload = function() { //Loads up the JavaScript after the page finishes loading
+
+
 //variable list for clairity
-var winCount; //counter for wins, starts at 0
-var lossCount; //counter for losses, starts at 0
-var guessesRemaining; //counter for Guesses, starts at 7
+var wCount = 0 // number of wins
+var lCount = 0 // number of losses
+var gCount = 8 // number of guesses upon start
 var currentLetter;
-var lettersGuessed
+var lettersGuessed = []
+var possibleLetters = [ 'a',
+                        'b',
+                        'c',
+                        'd',
+                        'e',
+                        'f',
+                        'g',
+                        'h',
+                        'i',
+                        'j',
+                        'k',
+                        'l',
+                        'm',
+                        'n',
+                        'o',
+                        'p',
+                        'q',
+                        'r',
+                        's',
+                        't',
+                        'u',
+                        'v',
+                        'w',
+                        'x',
+                        'y',
+                        'z']
 
 
 
-//Function to pick a random letter
+//writes intial guesses left value on the DOM
+document.getElementById('winCount').innerHTML = wCount;
+//writes intial Losses value on the DOM
+document.getElementById('lossCount').innerHTML = lCount;
+//writes intial Wins value on the DOM
+document.getElementById('guess').innerHTML = gCount;
 
 
-var guessesRemaining = 8
 
-document.onkeyup = function(event) {
-    var userKey = event.key;
+//Random Letter Generator Function
+function createLetter () {
+randLetter = possibleLetters[Math.floor(Math.random() * possibleLetters.length)];
+console.log(randLetter) }
 
 
-    var guessesRemaining = 8;
-    function subtract(){
-         guessesRemaining--;
-         document.getElementById('guessesRemaining').innerHTML = guessesRemaining;
+//calling function to create letter upon start
+createLetter();
+
+//Listening for KeyStrokes
+document.onkeyup = function(write) {
+    var userKey = write.key;
+
+
+    //Correct Guess / Win Behavior
+    if (userKey == randLetter) { 
+    alert("You Picked the Correct Letter, " + randLetter + " !")
+    gCount = 7;
+    wCount++;
+    lettersGuessed = [];
+    document.getElementById('guess').innerHTML = gCount;
+    document.getElementById('winCount').innerHTML = wCount;
+    document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
+    createLetter();
     }
 
 
+    // Incorrect Guesses Behavior 
+    else if (lettersGuessed.includes(userKey)) {} // ensures that letters are only used once
+    else if (possibleLetters.includes(userKey)) //ensures only valid characters (from array) are counted
+    {
+        gCount--;
+        document.getElementById('guess').innerHTML = gCount;
+        lettersGuessed.push(userKey);
+        document.getElementById('lettersGuessed').innerHTML = lettersGuessed
+     }
 
 
-    if (userKey == "h") { 
-    // display letter in current word
 
-    alert("h has been clicked")
-        }
-    else {
-    // increment "hangSteps" counter to change image
-        guessesRemaing - 1}
-
+     // Reset Upon Loss
+     if (gCount === 0){
+        lCount++;
+        document.getElementById('lossCount').innerHTML = lCount;
+        alert("You Lost a Round");
+        gCount = 7;
+        document.getElementById('guess').innerHTML = gCount;
+        lettersGuessed = []
+        document.getElementById('lettersGuessed').innerHTML = lettersGuessed
+        createLetter();
+    }
   }
 
+
+
+} //Closing Window On Load Function, this should be the end of all JS
